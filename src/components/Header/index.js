3319755b2aee048858f1d32d6da2377cc2e32f5e
Wendreo <wendreolf@gmail.com>
2019-06-22 00:13:42 -0300

    Adding vector-icons and images

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={} />
+        <Title>Wendreo</Title>
+      </Top>
+
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 40px;
+`;
